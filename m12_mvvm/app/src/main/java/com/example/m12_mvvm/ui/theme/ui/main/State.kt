bd4680e46338c5620de0f7b720a0063913add663
AlekhinVladimir <119034591+AlekhinVladimir@users.noreply.github.com>
2024-03-26 23:00:19 +0400

    Delete m12_mvvm directory
    херню сделал какую то

--- m12_mvvm/app/src/main/java/com/example/m12_mvvm/ui/theme/ui/main/State.kt
+++
@@ -1,7 +0,0 @@
-package com.example.m12_mvvm.ui.theme.ui.main
-
-sealed class State{
-    data object Loading : State()
-    data object Success : State()
-    data class Result(val value: String? = null) : State()
-}
